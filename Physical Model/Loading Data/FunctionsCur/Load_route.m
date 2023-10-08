96cdad3a19e63890302187b58894ece8f39eadc2
Sevmeyer01 <sevmeyer@ethz.ch>
2023-10-08 12:44:22 +0930

    Coding

--- Physical Model/Loading Data/FunctionsCur/Load_route.m
+++ Physical Model/Loading Data/FunctionsCur/Load_route.m
@@ -24,12 +24,33 @@ params = table2struct(Load_Parameters());
     route.cumDist = params.S_RW_vec;
 
     % Interpolating inclination vector
-    route.incl = interp1(route_raw.cumDistance,route_raw.inclinationSmoothed,route.cumDist);
+    incl = interp1(route_raw.cumDistance,route_raw.inclinationSmoothed,route.cumDist);
 
     % Interpolating maxV vector
-    route.maxV = interp1(route_raw.cumDistance,route_raw.maxSpeed,route.cumDist);
+    maxV = interp1(route_raw.cumDistance,route_raw.maxSpeed,route.cumDist);
+
+    % Determining group average constant n
+    n = params.S_step/params.S_RW_Step;
+
+    % Initialize empty 
+    route.incl = [];
+    route.maxV = [];
+
+    % Iterate through the original vector
+    for i = 1:n:length(route.cumDist)-n+1
+        % Extract the current group of 'n' values
+        group_incl = incl(i:i+n-1);
+        group_maxV = maxV(i:i+n-1);
+        
+        % Calculate the average of the group
+        averageValue_incl = mean(group_incl);
+        averageValue_maxV = mean(group_maxV);
+        
+        % Append the average value to the new vector
+        route.incl = [route.incl, averageValue_incl];
+        route.maxV = [route.maxV, averageValue_maxV];
+    end
 
     % Import Control Stops
     CS_raw = readtable('control_stops.csv');
     route.CS_cumDist = CS_raw.cumDistance(2:end-1);
-%end
